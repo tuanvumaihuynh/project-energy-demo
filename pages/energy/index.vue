5f01179005fb98b666812f1fb56a7ab2c497d308
lekien2k2 <tankien2k2@gmail.com>
2024-05-28 13:42:56 +0700

    chore: Refactor energy page to use Card component for device display

--- pages/energy/index.vue
+++ pages/energy/index.vue
@@ -2,11 +2,23 @@
   <div>
     hello
     <pre>
-        <code>{{ data }}</code>
+      <code>{{ data }}</code>
+        <Card v-for="device in data?.items" :key="device.id" class="col-md-4">
+          <CardHeader>
+            <Package class="w-8 h-8" />
+            <CardTitle>{{ device.name.split("|")[1] || device.name }}</CardTitle>
+            <Badge :class="device.connected == true ? 'bg-green-400' : 'bg-red-500'" variant="outline">{{ device.connected == true ? 'Connected':'Disconnected' }}</Badge>
+            <CardDescription>{{ device.description }}</CardDescription>
+          </CardHeader>
+          <Badge v-for="tags in device.tags" variant="outline" class="mr-1">{{ tags }}</Badge>
+          <CardContent>
+          </CardContent>
+          </Card>
       </pre>
   </div>
 </template>
 
 <script setup lang="ts">
+import { Package  } from 'lucide-vue-next';
 const { data } = await useAPI<ItemPagination<Device>>("/devices");
 </script>
