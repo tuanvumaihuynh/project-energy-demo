415e0298a6c177ea786a6eba6831f0b55769dfc7
tuanvumaihuynh <tuanvux01@gmail.com>
2024-05-28 10:49:29 +0700

    fix: add param query custom

--- composables/useCustomFetch.ts
+++ composables/useCustomFetch.ts
@@ -1,10 +1,13 @@
-export const useCustomFetch = (url: String) => {
+export const useCustomFetch = (url: String, options = {}) => {
   const config = useRuntimeConfig();
 
   const fullUrl = `${config.public.apiBase}${url}`;
   return useFetch(fullUrl, {
     headers: {
       "X-Api-Key": config.public.apiKey,
+    },
+    params: {
+      ...options,
     },
   });
 };
