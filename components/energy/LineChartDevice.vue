c91b617502eb82982d5ce4aad3745b95218b4bee
lekien2k2 <tankien2k2@gmail.com>
2024-05-31 16:49:24 +0700

    feat: Add Radix Vue components for popover and range calendar
    - Added new files for Radix Vue components: Popover, PopoverTrigger, PopoverContent, RangeCalendarGridBody, RangeCalendarGridHead, RangeCalendarGridRow
    - Updated package.json to include new dependencies: "@internationalized/date" and "pnpx"
    - Created new components: LineChartDevice, BarChartDevice, RangeCalendarGrid

---
+++ components/energy/LineChartDevice.vue
@@ -0,0 +1,33 @@
+<template>
+  <LineChart
+    :data="props.data"
+    index="ts"
+    :categories="['value']"
+    :x-formatter="
+      (tick, i) => {
+        return typeof tick === 'number'
+          ? new Date(tick).toLocaleDateString()
+          : '';
+      }
+    "
+    :y-formatter="
+      (tick, i) => {
+        return typeof tick === 'number'
+          ? `${new Intl.NumberFormat('us').format(tick).toString()} A`
+          : '';
+      }
+    "
+  />
+</template>
+
+<script setup lang="ts">
+import { LineChart } from "@/components/ui/chart-line";
+
+interface CurrentData {
+  ts: string;
+  value: number;
+}
+const props = defineProps<{
+  data: CurrentData[];
+}>();
+</script>
--- pages/energy/index.vue
+++ pages/energy/index.vue
@@ -3,22 +3,44 @@
     hello
     <pre>
       <code>{{ data }}</code>
-        <Card v-for="device in data?.items" :key="device.id" class="col-md-4">
-          <CardHeader>
-            <Package class="w-8 h-8" />
-            <CardTitle>{{ device.name.split("|")[1] || device.name }}</CardTitle>
-            <Badge :class="device.connected == true ? 'bg-green-400' : 'bg-red-500'" variant="outline">{{ device.connected == true ? 'Connected':'Disconnected' }}</Badge>
-            <CardDescription>{{ device.description }}</CardDescription>
-          </CardHeader>
-          <Badge v-for="tags in device.tags" variant="outline" class="mr-1">{{ tags }}</Badge>
-          <CardContent>
-          </CardContent>
-          </Card>
+        
       </pre>
+
+    <Card v-if="pending">
+      <CardHeader>
+        <Package class="w-8 h-8" />
+        <CardTitle>Loading...</CardTitle>
+      </CardHeader>
+    </Card>
+    <div class="grid grid-cols-4 gap-2">
+      <Card v-for="device in data?.items" :key="device.id">
+        <div class="flex">
+          <Package class="flex h-24 w-24 text-primary" />
+          <CardTitle class="flex">{{
+            device.name.split("|")[1] || device.name
+          }}</CardTitle>
+          <Badge v-for="tags in device.tags" variant="outline" class="mr-1">{{
+            tags
+          }}</Badge>
+        </div>
+        <Badge
+          :class="device.connected == true ? 'bg-green-400' : 'bg-red-500'"
+          variant="outline"
+          class="w-auto"
+          >{{ device.connected == true ? "Connected" : "Disconnected" }}</Badge
+        >
+        <CardDescription>{{ device.description }}</CardDescription>
+        <CardContent> </CardContent>
+      </Card>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Package  } from 'lucide-vue-next';
-const { data } = await useAPI<ItemPagination<Device>>("/devices");
+import { Package } from "lucide-vue-next";
+import LineChartDevice from "~/components/energy/LineChartDevice.vue";
+
+const { data, pending } = await useAPI<ItemPagination<Device>>("/devices", {
+  lazy: true,
+});
 </script>
